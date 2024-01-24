ca5603d5b3bf96250d8d502ab83aef2a5f4c8db2
FADHILI Josue <FADHILI Josue>
2024-01-24 14:44:35 +0200

    fix: typos

--- next.config.js
+++ next.config.js
@@ -10,9 +10,8 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
     dest: "public",
-    // disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    scope: '/app',
 });
 
 module.exports = withPWA(nextConfig);
--- src/components/country/BackButton.tsx
+++ src/components/country/BackButton.tsx
@@ -8,7 +8,7 @@ interface BackButtonProps {
 
 const BackButton: FC<BackButtonProps> = ({}) => {
   const router = useRouter()
-  return <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors disabled:opacity-50 shadow-md bg-slate-300 dark:bg-gray-700 hover:bg-slate-400 dark:hover:bg-gray-700/80 h-10 px-4 py-2"> <span className="font-bold text-xl mr-2" onClick={() => router.back()}>&larr;</span>back</button>
+  return <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors disabled:opacity-50 shadow-md bg-slate-300 dark:bg-gray-700 hover:bg-slate-400 dark:hover:bg-gray-700/80 h-10 px-4 py-2" onClick={() => router.back()}> <span className="font-bold text-xl mr-2">&larr;</span>back</button>
 }
 
 export default BackButton
